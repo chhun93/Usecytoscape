1152eed4851397a1529c668f5d2e72c9c18f9846
chhun93 <chhun3830@gmail.com>
2021-02-26 00:07:49 +0900

    webpack 경로 에러였음

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
+      template: "./index.html",
       filename: "index.html",
     }),
     new MiniCssExtractPlugin({
